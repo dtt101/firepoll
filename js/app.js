3827becbbf42a4356778885615d1191daec4a596
David Thompson <dtt101@gmail.com>
2013-12-30 12:52:56 +0000

    got postcode lookup working

--- js/app.js
+++ js/app.js
@@ -6,46 +6,42 @@ var app = app || {};
  * app setup and simple login handler
  */
 $(function() {
-  // set up auth
-  var chatRef = new Firebase('https://farmchat.firebaseio.com');
-  // callback from login
-  app.auth = new FirebaseSimpleLogin(chatRef, function(error, user) {
-    if (error) {
-      // an error occurred while attempting login
-      // TODO: handle this more elegantly
-      console.log(error);
-    } else if (user) {
-      // authentication success
-
-      // set up current user model from logged in user object
-      var currentUser = new app.LocalUser({
-        uid: user.uid,
-        id: user.id,
-        username: user.username,
-        provider: user.provider,
-        displayName: user.displayName
-      });
-      var userName = currentUser.get("username");
-
-      // both views only need to know what the current online user name is
-      // we pass in unique twitter id as userName 'i.e @twitter'
-      new app.RoomView({currentUserName: userName});
-      // set up user list
-      new app.UsersView({currentUserName: userName});
-
-      // Show main UI and hide login button
-      $('#login-container').addClass('hidden');
-      $('#roomview-container').removeClass('hidden');
-
-    } else {
-      // user is not logged in
-      console.log('user not logged in');
-    }
-  });
-
-  // setup login button event handler
-  $( "#login" ).on( "click", function(e) {
-    e.preventDefault();
-    app.auth.login('twitter');
-  });
+    
+    // create a view for postcode lookup
+    // do postcode lookup, if success then create pollview
+    // if not success show error
+    
+    // create a view for questions
+    //http://mikeygee.com/blog/backbone.html
+    
+    // handle find button click
+    $("#btnFind").on("click", function(e) {
+        e.preventDefault();
+        
+        // get postcode from form field
+        var postcode = $('#txtPostcode').val();
+        
+        // find parliamentary constituency from postcode using mapit api
+        var mapitAPI = "http://mapit.mysociety.org/postcode/" + postcode;
+        var wmcId = 0;
+        var wmcName = '';
+        $.getJSON(mapitAPI)
+            .done(function(data) {
+                $.each(data.areas, function(key, val) {
+                    if (val.type === 'WMC') {
+                        wmcId = val.id;
+                        wmcName = val.name;
+                    }
+                });
+                console.log(wmcId);
+                console.log(wmcName);
+                // TODO: show questions and populate constituency name and id
+                // TODO: create backbone view
+            })
+            .fail(function() {
+                // TODO handle error response and postcode not found
+                console.log("error fetching postcode");
+            });
+        
+    });
 });
